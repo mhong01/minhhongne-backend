3b336ad7c62700f52c37e18446f209def41cc670
Minh Hong <hellyeyoudo@gmail.com>
2020-11-09 21:18:14 -0800

    init backend

--- minhhongne/minhhongne_stack.py
+++ minhhongne/minhhongne_stack.py
@@ -1,9 +1,8 @@
 from aws_cdk import core
-
+from . import resume_service
 
 class MinhhongneStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
-        # The code that defines your stack goes here
+        resume_service.ResumeService(self, "ResumeApis")
