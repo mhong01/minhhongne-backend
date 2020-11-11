6ff9e661539d93e2d04936a0a607d0b20cc6e67a
Minh Hong <hellyeyoudo@gmail.com>
2020-11-10 22:59:32 -0800

    extensions

---
+++ minhhongne/resume_service.py
@@ -0,0 +1,48 @@
+from aws_cdk import (core,
+                     aws_apigateway as apigateway,
+                     aws_s3 as s3,
+                     aws_lambda as lambda_)
+
+class ResumeService(core.Construct):
+    def __init__(self, scope: core.Construct, id: str):
+        super().__init__(scope, id)
+
+        bucket = s3.Bucket(self, "ResumeStore")
+
+        handler = lambda_.Function(self, "ResumeHandler",
+                    runtime=lambda_.Runtime.NODEJS_10_X,
+                    code=lambda_.Code.asset("resources"),
+                    handler="resume.main",
+                    environment=dict(
+                    BUCKET=bucket.bucket_name)
+                    )
+
+        bucket.grant_read_write(handler)
+
+        api = apigateway.RestApi(self, "resume-api",
+                  rest_api_name="Resume Service",
+                  description="This service serves Resume Module.")
+
+        get_widgets_integration = apigateway.LambdaIntegration(handler,
+                request_templates={"application/json": '{ "statusCode": "200" }'})
+
+        api.root.add_method("GET", get_widgets_integration)   # GET /
+        print(id)
+        widget = api.root.add_resource("{id}")
+
+        # resumeData = api.root.add_resource('data')
+        # resumeData.add_method('POST')
+        # resumeData.add_method('GET')
+
+        # Add new widget to bucket with: POST /{id}
+        post_widget_integration = apigateway.LambdaIntegration(handler)
+
+        # Get a specific widget from bucket with: GET /{id}
+        get_widget_integration = apigateway.LambdaIntegration(handler)
+
+        # Remove a specific widget from the bucket with: DELETE /{id}
+        delete_widget_integration = apigateway.LambdaIntegration(handler)
+
+        widget.add_method("POST", post_widget_integration);     # POST /{id}
+        widget.add_method("GET", get_widget_integration);       # GET /{id}
+        widget.add_method("DELETE", delete_widget_integration); # DELETE /{id}
